917124a1c9333891b4dcf9ec19a49d7c2e2a641b
Meysam zarghami <m43z@hotmail.com>
2021-02-27 19:34:04 +0330

    Add Base MVVM For New Brench

---
+++ Restaurant_ManagementFull/ViewModel/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace Restaurant_ManagementFull.ViewModel
+{
+  public class BaseViewModel : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChenged<T>(ref T fild, T value, [CallerMemberName] string PropertyName = null)
+    {
+      fild = value;
+      if (!EqualityComparer<T>.Default.Equals(fild, value))
+      {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+      }
+    }
+  }
+}
---
+++ Restaurant_ManagementFull/ViewModel/RellayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Restaurant_ManagementFull.ViewModel
+{
+  public class RellayCommand : ICommand
+  {
+    public event EventHandler CanExecuteChanged;
+    private readonly Action<object> _execute;
+    private readonly Predicate<object> _canExecute;
+
+    public void RiseCanExecute()
+    {
+      CanExecuteChanged?.Invoke(this, new EventArgs());
+    }
+
+    public RellayCommand(Action<object> execute, Predicate<object> canExecute)
+    {
+      _execute = execute;
+      _canExecute = canExecute;
+    }
+
+    public RellayCommand(Action<object> execute)
+    {
+      _execute = execute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+      return _canExecute == null ? true : _canExecute(parameter);
+    }
+
+    public void Execute(object parameter)
+    {
+      _execute(parameter);
+    }
+  }
+}
